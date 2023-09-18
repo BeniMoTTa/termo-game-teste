6f314c8b7fb21c71a7fd9dcce77efa1b0a33db61
Jozhué Beni <jbeeniif@gmail.com>
2023-09-18 16:16:59 -0300

    CSS command

--- termo-game/src/components/WordleGame.tsx
+++ termo-game/src/components/WordleGame.tsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 
-const WORD_TO_GUESS = "HELLO";
+export function WordleGame() {
+  const WORD_TO_GUESS = "HELLO";
+  const MAX_ATTEMPTS = 6;
 
-function WordleGame() {
   const [guess, setGuess] = useState(["", "", "", "", ""]);
   const [feedback, setFeedback] = useState(["", "", "", "", ""]);
   const [attempts, setAttempts] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
   const handleInputChange = (
     index: number,
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const value = event.target.value.toUpperCase();
-    const newGuess = [...guess];
-    newGuess[index] = value;
-    setGuess(newGuess);
+    if (!gameOver) {
+      const value = event.target.value.toUpperCase();
+      const newGuess = [...guess];
+      newGuess[index] = value;
+      setGuess(newGuess);
+    }
   };
 
   const checkGuess = () => {
+    if (gameOver) return;
+
     const newAttempts = attempts + 1;
     const newFeedback = [];
 
@@ -38,6 +44,9 @@ function WordleGame() {
       alert(
         `Parabéns! Você adivinhou a palavra "${WORD_TO_GUESS}" em ${newAttempts} tentativas.`
       );
+    } else if (newAttempts >= MAX_ATTEMPTS) {
+      alert(`Game Over! A palavra correta era "${WORD_TO_GUESS}".`);
+      setGameOver(true);
     }
   };
 
@@ -47,7 +56,7 @@ function WordleGame() {
         {feedback.map((f, index) => (
           <div
             key={index}
-            className={`w-14 h-14 p-4 rounded-full border bg-[white] border-black m-2 ${
+            className={`w-16 h-16 p-4 rounded-full border bg-[white] border-black m-2 ${
               f === "correct"
                 ? "bg-green-400"
                 : f === "wrongPosition"
@@ -65,20 +74,20 @@ function WordleGame() {
             key={index}
             type="text"
             maxLength={1}
-            className="w-12 h-12 p-2 text-3xl rounded-lg border border-black mx-2"
+            className="w-16 h-16 p-2 text-3xl rounded-lg border border-black mx-2"
             value={g}
             onChange={(event) => handleInputChange(index, event)}
+            disabled={gameOver}
           />
         ))}
       </div>
       <button
         className="mt-8 px-4 py-2 bg-blue-500 text-white font-bold rounded"
         onClick={checkGuess}
+        disabled={gameOver}
       >
         Submit Guess
       </button>
     </div>
   );
 }
-
-export default WordleGame;
