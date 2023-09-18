7e828a337a97e5c63002a420d8eabbb7e32f0d03
Jozhué Beni <jbeeniif@gmail.com>
2023-09-18 17:13:14 -0300

    Initiate commit

--- termo-game/src/components/WordleGame.tsx
+++ termo-game/src/components/WordleGame.tsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 
-export function WordleGame() {
-  const WORD_TO_GUESS = "HELLO";
-  const MAX_ATTEMPTS = 6;
+const WORD_TO_GUESS = "HELLO";
+const MAX_ATTEMPTS = 6;
 
+export function WordleGame() {
   const [guess, setGuess] = useState(["", "", "", "", ""]);
   const [feedback, setFeedback] = useState(["", "", "", "", ""]);
   const [attempts, setAttempts] = useState(0);
   const [gameOver, setGameOver] = useState(false);
+  const [activeRow, setActiveRow] = useState(0);
 
-  const handleInputChange = (
-    index: number,
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (!gameOver) {
-      const value = event.target.value.toUpperCase();
+  const handleInputChange = (index: number, value: string) => {
+    if (!gameOver && activeRow < guess.length) {
       const newGuess = [...guess];
-      newGuess[index] = value;
+      newGuess[activeRow] = value.toUpperCase();
       setGuess(newGuess);
     }
   };
@@ -39,16 +36,20 @@ export function WordleGame() {
 
     setFeedback(newFeedback);
     setAttempts(newAttempts);
+    setActiveRow(activeRow + 1);
 
     if (guess.join("") === WORD_TO_GUESS) {
       alert(
         `Parabéns! Você adivinhou a palavra "${WORD_TO_GUESS}" em ${newAttempts} tentativas.`
       );
+      setGameOver(true);
     } else if (newAttempts >= MAX_ATTEMPTS) {
       alert(`Game Over! A palavra correta era "${WORD_TO_GUESS}".`);
       setGameOver(true);
     }
   };
+
+  const keyboardLetters = "QWERTYUIOPASDFGHJKLÇZXCVBNM";
 
   return (
     <div className="text-center mt-12">
@@ -56,7 +57,7 @@ export function WordleGame() {
         {feedback.map((f, index) => (
           <div
             key={index}
-            className={`w-16 h-16 p-4 rounded-full border bg-[white] border-black m-2 ${
+            className={`w-16 h-16 p-4 border bg-[white] border-black m-2 ${
               f === "correct"
                 ? "bg-green-400"
                 : f === "wrongPosition"
@@ -69,22 +70,31 @@ export function WordleGame() {
         ))}
       </div>
       <div className="mt-4">
-        {guess.map((g, index) => (
-          <input
-            key={index}
-            type="text"
-            maxLength={1}
-            className="w-16 h-16 p-2 text-3xl rounded-lg border border-black mx-2"
-            value={g}
-            onChange={(event) => handleInputChange(index, event)}
-            disabled={gameOver}
-          />
-        ))}
+        <div className="flex flex-wrap justify-center">
+          <div className="flex flex-wrap w-[700px] text-[white]">
+            {Array.from(keyboardLetters).map((letter, index) => (
+              <button
+                key={index}
+                className="w-12 h-12 p-2 text-3xl rounded-lg border-2 border-black m-2"
+                onClick={() => handleInputChange(activeRow, letter)}
+                disabled={
+                  gameOver || activeRow >= guess.length || !!guess[activeRow]
+                }
+              >
+                {letter}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
       <button
         className="mt-8 px-4 py-2 bg-blue-500 text-white font-bold rounded"
         onClick={checkGuess}
-        disabled={gameOver}
+        disabled={
+          gameOver ||
+          activeRow >= guess.length ||
+          guess[activeRow].trim() === ""
+        }
       >
         Submit Guess
       </button>
