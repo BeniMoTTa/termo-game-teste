d454693c124b06b13247124afed2bcbe90150155
Jozhué Beni <jbeeniif@gmail.com>
2023-09-20 15:54:11 -0300

    refatoring code to make more dinamical

--- termo-game/src/App.tsx
+++ termo-game/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="bg-[#084D6E] main flex flex-col">
         <header className="main-header flex w-full justify-center text-[55px] font-bold mt-[25px] ">
-          <div className="p-1 border-2 border-[black] background rounded-xl">
+          <div className="p-1 border-2 mb-[50px] border-[black] background rounded-xl">
             <h1>
               <span className="title-word title-word-1">Wordle</span>
               <span className="title-word title-word-2"> / </span>
--- termo-game/src/components/WordleGame.tsx
+++ termo-game/src/components/WordleGame.tsx
@@ -1,111 +1,41 @@
 import React, { useEffect, useState } from "react";
 
 const WordleGame = () => {
-  const [palavraSecreta, setPalavraSecreta] = useState(gerarPalavra());
-  const [tentativa, setTentativa] = useState("");
-  const [resultado, setResultado] = useState<string | null>("");
-  const [tentativasRealizadas, setTentativasRealizadas] = useState(0);
+  const [letters, setLetters] = useState<string[]>([]);
+  const maxLetters = 5;
+  const numRows = 5;
 
-  function gerarPalavra() {
-    const palavras = [
-      "sabio",
-      "furia",
-      "pleno",
-      "fazer",
-      "caber",
-      "julho",
-      "treva",
-      "amora",
-      "pauta",
-      "labio",
-    ];
-    return palavras[Math.floor(Math.random() * palavras.length)];
-  }
-  const verificarTentativa = () => {
-    if (tentativa === palavraSecreta) {
-      setResultado("Você venceu! A palavra era " + palavraSecreta);
-    } else {
-      const letrasCorretas = [];
-      const letrasIncorretas = [];
-      for (let i = 0; i < tentativa.length; i++) {
-        if (palavraSecreta.includes(tentativa[i])) {
-          if (palavraSecreta[i] === tentativa[i]) {
-            letrasCorretas.push(tentativa[i]);
-          } else {
-            letrasIncorretas.push(tentativa[i]);
-          }
-        }
-      }
-      setResultado(null);
-    }
-    setTentativasRealizadas(tentativasRealizadas + 1);
-  };
   useEffect(() => {
-    const tentativasRows = [];
-    for (let i = 0; i < tentativasRealizadas + 1; i++) {
-      tentativasRows.push(
-        <div key={i} className="flex space-x-2 mt-2">
-          {[...Array(5)].map((_, j) => (
-            <div
-              key={j}
-              className={
-                i < tentativasRealizadas
-                  ? "w-6 h-6 bg-gray-300 rounded"
-                  : "w-6 h-6 bg-white rounded"
-              }
-            >
-              {i < tentativasRealizadas && tentativa[j] ? tentativa[j] : null}
-            </div>
-          ))}
-        </div>
-      );
-    }
-    setResultado(resultado);
-    setTentativa(tentativa);
-    return () => {
-      setPalavraSecreta("");
-      setTentativa("");
-      setResultado(null);
-      setTentativasRealizadas(0);
+    const handleKeyPress = (e: KeyboardEvent) => {
+      const { key } = e;
+      if (key.match(/[a-zA-Z]/) && letters.length < maxLetters) {
+        setLetters((prevLetters) => [...prevLetters, key]);
+      }
     };
-  }, [tentativa, resultado, tentativasRealizadas]);
-  const renderTentativas = () => {
-    const tentativasRows = [];
-    for (let i = 0; i < tentativasRealizadas + 1; i++) {
-      tentativasRows.push(
-        <div key={i} className="flex space-x-2 mt-2">
-          {[...Array(5)].map((_, j) => (
-            <div key={j} className="w-6 h-6 bg-gray-300 rounded"></div>
-          ))}
-        </div>
-      );
-    }
-    return tentativasRows;
-  };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [letters]);
   return (
-    <div className="container mx-auto text-center mt-8">
-      <h1 className="text-3xl font-semibold mb-4">Wordle Game</h1>
-      <p>Adivinhe a palavra secreta de 5 letras.</p>
-      <div className="flex flex-wrap justify-center mt-4">
-        {[...Array(5)].map((_, j) => (
-          <input
-            type="text"
-            maxLength={1}
-            key={j}
-            className="border p-2 mt-4"
-            onChange={(e) => setTentativa(e.target.value)}
-          />
-        ))}
+    <>
+      <div className="App">
+        <div className="w-[50%] allign">
+          <div className="grid grid-cols-5 gap-1">
+            {Array.from({ length: maxLetters * numRows }).map((_, index) => (
+              <div
+                key={index}
+                className="w-28 h-28 mb-2 bg-blue-500 text-white text-2xl flex items-center justify-center rounded"
+              >
+                {letters[index]}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <button
-        className="bg-blue-500 text-white p-2 mt-2 hover:bg-blue-600 rounded"
-        onClick={verificarTentativa}
-      >
-        Verificar
-      </button>
-      <p className="mt-4">{resultado}</p>
-      <div className="mt-4">{renderTentativas()}</div>
-    </div>
+    </>
   );
 };
 
